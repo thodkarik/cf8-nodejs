02ea0aee8d7a9d086714cac70a87b1e50b866e69
thodkarik <thod.karik@gmail.com>
2026-03-22 12:02:41 +0200

    minor

--- myapp3/src/app.ts
+++ myapp3/src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import {greet} from "./utils";
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+app.get('/greetings', (req, res) => {
+    const message = greet("Rigio");
+    res.send(message);
+})
 
 app.listen(3000, () => {
     return console.log("Server is up");
